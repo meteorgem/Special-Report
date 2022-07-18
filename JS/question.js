e24ff77b60b31bca0f0c3ee555118de5585f77f2
meteorgem <qseawd87512360@gmail.com>
2022-07-18 09:20:09 +0800

    JS修改

--- JS/question.js
+++ JS/question.js
@@ -1,17 +1,17 @@
 
-$(".single-acco h2").click(function(){
+$(".question-middle-inder h2").click(function(){
    
-    if($(this).next(".accodion-body").hasClass("active")){
-      $(this).next(".accodion-body").removeClass("active").slideUp();
+    if($(this).next(".question-middle-on").hasClass("active")){
+      $(this).next(".question-middle-on").removeClass("active").slideUp();
      
       $(this).children("span").html('+');   
     }
     else{
-      $(".accodion-body").removeClass("active").slideUp();
+      $(".question-middle-on").removeClass("active").slideUp();
       
-      $(".single-acco h2 span").html('+');
+      $(".question-middle-inder span").html('+');
   
-      $(this).next(".accodion-body").addClass("active").slideDown();
+      $(this).next(".question-middle-on").addClass("active").slideDown();
       
       $(this).children("span").html('-');
     }
