20def5e1648ddc8718432faa2361453f32bf4590
meteorgem <qseawd87512360@gmail.com>
2022-08-02 13:27:38 +0800

    ADD 漢堡修改

--- JS/index.js
+++ JS/index.js
@@ -1,7 +1,16 @@
 $(document).ready(function() {
-	$('#Hamburger').on('click', function(event) {
-		event.preventDefault();
+	$('#Hamburger').on('click', function(e) {
+		e.preventDefault();
 		$('.header-in').toggleClass('active');
+		$('.header-top-in').toggleClass('battle');
+	});
+	
+	$('.header-top-in').on('click', function() {
+		$('.header-top-in').removeClass('battle');
+		$('.header-in').removeClass('active');
 	});
 
+	
+
 });
+
